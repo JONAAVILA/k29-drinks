b6b2ad1e9875864cedc796f69597fe228c519fab
JONAAVILA <jona_1969@hotmail.com>
2025-01-10 10:12:24 -0300

    navbar fixed and shop button created

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,20 @@
+import Shop from '../buttons/shop/Shop'
 import './navbar.css'
 
 const Navbar = ()=>{
     return(
         <aside className='navbar_container' >
-            <div>
-                <a href="">INGRESO</a>
-                <a href="">REGISTRARSE</a>
-            </div>
-            <div>
-                <a href="">ENVIOS</a>
-                <a href="">CONTACTO</a>
-                <a href="">PRODUCTOS</a>
+            <div className='navbar_box' >
+                <div className='navbar_signin' >
+                    <a href="">INGRESO</a>
+                    <a href="">REGISTRARSE</a>
+                </div>
+                <div className='navbar_contact' >
+                    <a href="">ENVIOS</a>
+                    <a href="">CONTACTO</a>
+                    <a href="">PRODUCTOS</a>
+                    <Shop/>
+                </div>
             </div>
         </aside>
     )
